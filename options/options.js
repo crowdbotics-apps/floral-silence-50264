4d63a47de8b7422bce80514a1663e5585ebec21c
Crowdbotics <team@crowdbotics.com>
2024-12-10 15:20:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-silence-50264",
+  url: "https://floral-silence-50264.botics.co",
+  api: "https://floral-silence-50264.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
